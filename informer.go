f13576948a1f1516885d08c74bb41fa713a0cbd8
prumor <prumor@inventa.com>
2019-09-06 12:42:29 +0100

    pointer defaults conflit

--- informer.go
+++ informer.go
@@ -62,9 +62,9 @@ type Configuration struct {
 }
 
 //NewLogger returns an instance of Logger
-func NewLogger(config *Configuration, loggerInstance int) error {
+func NewLogger(config Configuration, loggerInstance int) error {
 
-	if err := defaults.Set(config); err != nil {
+	if err := defaults.Set(&config); err != nil {
 		panic(err)
 	}
 
